7bef38a9249db58c0133980116c7d59269744310
Juan Pablo Yepes <juanpabloyepest@gmail.com>
2020-06-13 16:05:02 +0000

    Task 2 - Selection sort

--- sort.h
+++ sort.h
@@ -2,7 +2,6 @@
 #define SORT_H
 
 #include <stdlib.h>
-#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
@@ -22,6 +21,5 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
-
 
 #endif
