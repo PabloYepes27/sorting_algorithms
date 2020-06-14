10bf995bcf4b757a67cbf1b982fc2117488c3f71
PabloYepes27 <juanpabloyepest@gmail.com>
2020-06-14 18:14:54 +0000

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,21 +8,26 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, index;
-	int tmp, swap;
+	int tmp, swap, flag = 0;
 
 	if (array == NULL)
 		return;
 	for (i = 0; i < size; i++)
 	{
 		tmp = i;
+		flag = 0;
 		for (index = i + 1; index < size; index++)
 		{
 			if (array[tmp] > array[index])
+			{
 				tmp = index;
+				flag += 1;
+			}
 		}
 		swap = array[i];
 		array[i] = array[tmp];
 		array[tmp] = swap;
-		print_array(array, size);
+		if (flag != 0)
+			print_array(array, size);
 	}
 }
