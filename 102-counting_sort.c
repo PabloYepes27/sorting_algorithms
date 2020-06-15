b0e73af22a5c40df8ca97bb1b89dba0fad95c70c
angellovc <vc.angel@outlook.com>
2020-06-15 17:25:58 +0000

    New Feat: counting sort algorithm was added
    counting sort algorithm was added into 102-countint_sort.c
    102-O file represent the big O cases
    sort.h was updated with sort algorithm prototypes

---
+++ 102-counting_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
+}
+/**
+ * counting_sort - this is a counting sort method implementation
+ * @array: array to sort
+ * @size: array size
+ */
+void counting_sort(int *array, size_t size)
+{
+	int index, maximun = 0, *counter = '\0', *tmp = '\0';
+	size_t i;
+
+	/* find maximun number */
+	for (i = 0; i < size; i++)
+		if (array[i] > maximun)
+			maximun = array[i];
+	counter = _calloc(maximun + 1, sizeof(int));
+	tmp = _calloc(size + 1, sizeof(int));
+	/* count the array elements */
+	for (i = 0; i < size; i++)
+		counter[array[i]]++;
+	/* get the accumulative values */
+	for (index = 1; index <= maximun; index++)
+		counter[index] += counter[index - 1];
+	print_array(counter, maximun + 1);
+	/* get the new array sorted */
+	for (i = 0; i < size; ++i)
+		tmp[counter[array[i]] - 1] = array[i], counter[array[i]]++;
+	/* replace old array to new array sorted */
+	for (i = 0; i < size; i++)
+		array[i] = tmp[i];
+	free(tmp);
+	free(counter);
+
+}
