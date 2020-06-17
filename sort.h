569b515c7ca0ac73f0f9a9dabe1746ca7c163b54
angellovc <vc.angel@outlook.com>
2020-06-17 18:36:10 +0000

    New feat: Bitonic sort implementation was added into 106-bitonic_sort.c
    sort.h was updated
    106-O have the big O notation of Bitonic sort

--- sort.h
+++ sort.h
@@ -29,5 +29,6 @@ void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 
 #endif
