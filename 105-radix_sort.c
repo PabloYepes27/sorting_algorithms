f7dd872b1175e106ee17db930e1c6f3a894abba9
PabloYepes27 <juanpabloyepest@gmail.com>
2020-06-17 00:10:58 +0000

    Task 9

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,6 +1,25 @@
 #include "sort.h"
 #include <stdio.h>
+/**
+ *_calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
 
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
+}
 /**
 * getMax - A utility function to get maximum value in arr[]
 * @arr: array
@@ -9,12 +28,12 @@
 */
 int getMax(int arr[], int n)
 {
-	int i, mx = arr[0];
+	int i, max = arr[0];
 
 	for (i = 1; i < n; i++)
-		if (arr[i] > mx)
-			mx = arr[i];
-	return (mx);
+		if (arr[i] > max)
+			max = arr[i];
+	return (max);
 }
 
 /**
@@ -22,13 +41,14 @@ int getMax(int arr[], int n)
 * the digit represented by exp.
 * @arr: array
 * @n: size of the array
-* @exp: Do counting sort for every digit
+* @exp: exp is 10^i
 */
 void countSort(int arr[], int n, int exp)
 {
-	int output[1024]; /* output array should be n(size) */
+	int *output = '\0'; /* output array should be n(size) */
 	int i, count[10] = {0};
 
+	output = _calloc(n + 1, sizeof(int));
 	/* Store count of occurrences in count[] */
 	for (i = 0; i < n; i++)
 		count[(arr[i] / exp) % 10]++;
@@ -54,6 +74,7 @@ void countSort(int arr[], int n, int exp)
 	for (i = 0; i < n; i++)
 		arr[i] = output[i];
 	print_array(arr, n);
+	free(output);
 }
 
 /**
@@ -65,13 +86,15 @@ void countSort(int arr[], int n, int exp)
 void radix_sort(int *array, size_t size)
 {
 	/* Find the maximum number to know number of digits */
-	int exp, m = getMax(array, size);
+	int exp, maximum = getMax(array, size);
 
+	if (array == '\0' || size < 2)
+		return;
 	/*
 	* Do counting sort for every digit. Note that instead
     * of passing digit number, exp is passed. exp is 10^i
     * where i is current digit number
 	*/
-	for (exp = 1; m / exp > 0; exp *= 10)
+	for (exp = 1; maximum / exp > 0; exp *= 10)
 		countSort(array, size, exp);
 }
